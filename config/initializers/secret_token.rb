8e7381ebdb5975a5275fca94821f1c80af8088a2
x3stick <x3stick@gmail.com>
2013-07-06 21:05:50 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aywed::Application.config.secret_key_base = '7757817394e01090067621066b6844d407339b4ecdd8f89c3cedbe038675dc5da6652faeeee119b7654b6e40b9cc884128bad826a724beb1b85b656cd4e6e3d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Aywed::Application.config.session_store :cookie_store, key: '_aywed_session'
